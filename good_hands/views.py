81ff9c4c6a820dd02a0ce5a56e2fcb8c383e5cb0
FilipLangner <filip.langner@gmail.com>
2020-04-28 13:16:25 +0200

    Adding dynamic counting of donation quantity and institutions supported

--- good_hands/views.py
+++ good_hands/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.db.models import Sum
 from django.views import generic
+
+from good_hands.models import Institution, Donation
 
 
 class LandingPageView(generic.TemplateView):
     template_name = "good_hands/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['institutions_count'] = Institution.objects.all().count()
+        context['donation_qty'] = Donation.objects.all().aggregate(Sum('quantity'))
+        return context
 
 class LoginView(generic.TemplateView):
     template_name = "good_hands/login.html"
